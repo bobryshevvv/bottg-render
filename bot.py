e91e65a6e914c1ad4b7c7feb1a71031ce04f09e7
bobryshevvv <bobryshev9898@gmail.com>
2025-04-16 22:16:38 +0300

    Первый коммит

--- bot.py
+++ bot.py
@@ -1,26 +1,29 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import os
 
-# Токен, который ты получил от BotFather
-API_TOKEN = 8005566639:AAHgfTudSiSR-v7I2DI7PYQ1ZAoyxy3cJjA
+TOKEN = os.environ.get(8005566639:AAHgfTudSiSR-v7I2DI7PYQ1ZAoyxy3cJjA)
 
-# Функция для команды /start
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет! Я твой бот.')
+menu_keyboard = [["Записаться", "Частые вопросы"]]
+markup = ReplyKeyboardMarkup(menu_keyboard, resize_keyboard=True)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Я бот Федерации эстетической гимнастики Курска.", reply_markup=markup)
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    if text == "Записаться":
+        await update.message.reply_text("Чтобы записаться, напишите имя родителя, имя ребёнка и возраст.")
+    elif text == "Частые вопросы":
+        await update.message.reply_text("С 4 лет, ул. Литовская 12, тренеры: Бобрышева В.А., Королева И.А., Собенникова Е.С., группы есть бюджетные.")
+    else:
+        await update.message.reply_text("Не понял команду 😅")
 
 def main():
-    # Создаём объект Updater с твоим токеном
-    updater = Updater(API_TOKEN, use_context=True)
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.run_polling()
 
-    # Получаем диспетчер для добавления обработчиков
-    dispatcher = updater.dispatcher
-
-    # Добавляем обработчик для команды /start
-    dispatcher.add_handler(CommandHandler("start", start))
-
-    # Запускаем бота
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
