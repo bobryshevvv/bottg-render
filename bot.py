100b03f4ceede2b15d01b1b053e4f4be60b369dc
bobryshevvv <bobryshev9898@gmail.com>
2025-04-16 22:36:08 +0300

    Четвертый коммит

--- bot.py
+++ bot.py
@@ -1,28 +1,21 @@
-from telegram import Update, ReplyKeyboardMarkup
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 import os
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-menu_keyboard = [["Записаться", "Частые вопросы"]]
-markup = ReplyKeyboardMarkup(menu_keyboard, resize_keyboard=True)
+logging.basicConfig(level=logging.INFO)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я бот Федерации эстетической гимнастики Курска.", reply_markup=markup)
-
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
-    if text == "Записаться":
-        await update.message.reply_text("Чтобы записаться, напишите имя родителя, имя ребёнка и возраст.")
-    elif text == "Частые вопросы":
-        await update.message.reply_text("С 4 лет, ул. Литовская 12, тренеры: Бобрышева В.А., Королева И.А., Собенникова Е.С., группы есть бюджетные.")
-    else:
-        await update.message.reply_text("Не понял команду 😅")
+    await update.message.reply_text("Привет! Я бот Федерации эстетической гимнастики Курска! 👋")
 
 def main():
-    app = Application.builder().token(TOKEN).build()
+    if not BOT_TOKEN:
+        raise ValueError("BOT_TOKEN не найден. Убедитесь, что переменная окружения установлена.")
+
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.run_polling()
 
 if __name__ == "__main__":
